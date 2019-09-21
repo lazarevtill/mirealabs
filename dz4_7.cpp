1035688bd949fa2375258fa40a0f8a1f4b6e76d3
lazarevtill <lazarevl2001@gmail.com>
2019-09-21 21:45:13 +0300

    Add files via upload

--- dz4_7.cpp
+++ dz4_7.cpp
@@ -3,21 +3,23 @@
 #include <ctime> 
 #include <cmath>
 
-
 using namespace std;
 
 int main()
 {
+	float s1;
 	float randomDigits_5[10]{}; // для чисел c плавающей точкой
 	int m, i{}, c{};
 	float s;
 	m = 37; i = 3; c = 64;
-	cin >> s;
+	cin >> s1;
 	srand(time(NULL));
 	for (int i = 0; i < 10; i++)
 	{
-		randomDigits_5[i] = fmodf((m * s + i), c);
+		randomDigits_5[i] = fmodf((m * s1 + i), c);
+		s1 = randomDigits_5[i];
 	}
+
 
 
 	cout << endl << "Массив c числами: ";
