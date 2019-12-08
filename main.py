360135030f4ebfac5669a4970a1a4528c4447301
lazarevtill <lazarevl2001@gmail.com>
2019-12-08 01:35:44 -0800

    Add files via upload
    inf last task

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+x: int
+y: int
+x = y = 0
+print("Input rows")
+x = int(input())
+print("Input columns")
+y = int(input())
+a = np.zeros(x)
+check: int
+if 1 < x < 6:
+    if 1 < y < 6:
+        mas = np.zeros((x, y))
+        print("1 if yourself input 0 if auto")
+        check = int(input())
+        if check == 1:
+            print("input letter " + str(x * y) + " times")
+            for i in range(x):
+                for j in range(y):
+                    mas[i][j] = int(ord(input()))
+        else:
+            for i in range(x):
+                for j in range(y):
+                    mas[i][j] = 65 + i + j
+        for i in range(x):
+            for j in range(y):
+                test = mas[i][j]
+                if test == 65 or test == 69 or test == 73 or test == 79 or test == 85 or test == 89 or test == 97 or test == 101 or test == 105 or test == 111 or test == 117 or test == 121:
+                    a[i] += 10
+                else:
+                    a[i] += 1
+        a.sort()
+        print(a)
+    else:
+        print("Wrong input")
+else:
+    print("Wrong input")
